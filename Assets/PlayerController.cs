45d1a9767c6e38f94b956d4429e956c0a9c392df
Alastair <ally@hvitt.co.uk>
2018-04-29 00:41:58 +0100

    best branch

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print ("hello my dear");
+
+		var rot = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+		var v = Input.GetAxis("Vertical") * Time.deltaTime * 150.0f;
+
+		print(rot);
+		transform.Rotate(0,0,rot);
+		transform.position += new Vector3(v,0,0);
+		
+	}
+}
